92e85cbb6cd464d941ad9ef8428998464405be48
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-06-11 15:57:53 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+var App = window.App = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/views/*');
+require('scripts/router');
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
